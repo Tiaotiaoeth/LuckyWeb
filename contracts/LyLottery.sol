2a1e714da20e764f473c621cc89b45072ac5c0c8
RyanDGWEB3 <136770240+RyanDGWEB3@users.noreply.github.com>
2023-07-10 20:46:18 +0800

    Update LyLottery.sol
    remove localId

--- contracts/LyLottery.sol
+++ contracts/LyLottery.sol
@@ -6,15 +6,12 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract LyLottery is ERC721, ERC721Burnable, Ownable {
+contract LyLottery is ERC721, Ownable {
 
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
-    Counters.Counter private _localIdCounter;
 
-    // from global token id to local id
-    mapping(uint256 => uint256) private _issueLotteryId;
     // administrator address
     address private _admin;
 
@@ -31,7 +28,6 @@ contract LyLottery is ERC721, ERC721Burnable, Ownable {
     }
 
     function balanceOf(address owner) public onlyOwner view override returns (uint256) {
-        // the number of tokens
         return ERC721.balanceOf(owner);
     }
 
@@ -45,23 +41,18 @@ contract LyLottery is ERC721, ERC721Burnable, Ownable {
         return _ownerOf(tokenId) == owner;
     }
 
-    function reset() public {
-        require(_admin == msg.sender, "NotAllowed");
-        _localIdCounter.reset();
-        _localIdCounter.increment();
-    }
-
     // return global token id and local issue lottery id
-    function safeMint(address to) public returns (uint256, uint256) {
+    function safeMint(address to) public returns (uint256) {
         require(_admin == msg.sender, "NotAllowed");
 
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
 
-        uint256 localId = _localIdCounter.current();
-        _localIdCounter.increment();
-        _issueLotteryId[tokenId] = localId;
-        return (tokenId, localId);
+        return tokenId;
+    }
+
+    function getMaxTokenId() public view returns (uint256) {
+        return _tokenIdCounter.current();
     }
 }
