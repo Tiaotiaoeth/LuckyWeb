7f341c8d60cd4d1f495f0f542a196f5204cc4532
RyanDGWEB3 <136770240+RyanDGWEB3@users.noreply.github.com>
2023-07-14 11:51:47 +0800

    Update LyIssuer.sol
    recover getLotteryNum by user and issue number

--- contracts/LyIssuer.sol
+++ contracts/LyIssuer.sol
@@ -412,4 +412,16 @@ contract LyIssuer is Ownable {
         return curIssue.minLotteryNFTId;
     }
 
+    /* get lotteries by issue number */
+    function getLotteryNum(uint256 issueNum) external view returns (uint256) {
+        uint256 lotteryNum = 0;
+        address sender = msg.sender;
+        uint256[] memory rewards = _issues[issueNum].rewards;
+        for (uint i = 0; i < rewards.length; i++) {
+            if (_lyLottery.checkOwnership(sender, rewards[i])) {
+                lotteryNum += 1;
+            }
+        }
+        return lotteryNum;
+    }
 }
