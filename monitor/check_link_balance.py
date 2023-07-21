291f96d4265e2d3ad2e588088c80530391306f7c
RyanDGWEB3 <136770240+RyanDGWEB3@users.noreply.github.com>
2023-07-21 10:38:18 +0800

    Update check_link_balance.py

--- monitor/check_link_balance.py
+++ monitor/check_link_balance.py
@@ -5,10 +5,10 @@ def check_link_balance():
     w3 = Web3(Web3.HTTPProvider('https://goerli-rollup.arbitrum.io/rpc'))
 
     link_addr = '0xd14838A68E8AFBAdE5efb411d5871ea0011AFd28'
-    link_abi = open('./monitor/LinkToken.abi').read()
+    link_abi = open('./monitor/LinkToken.ABI').read()
     link_token = w3.eth.contract(address=link_addr, abi=link_abi)
 
-    vrfv2_addr = '0x5387161c011850E69e36dE5B7be7d55B90d86994'
+    vrfv2_addr = '0x7D67652A6FED4B935bDc496A4717a9B9cf90e725'
     balance = link_token.functions.balanceOf(vrfv2_addr).call()
     balance = w3.from_wei(balance, 'ether')
     print('Link balance', balance)
