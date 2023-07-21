8dbcc27ac242a105e3b7ca80f9f794010bc2d147
RyanDGWEB3 <136770240+RyanDGWEB3@users.noreply.github.com>
2023-07-21 20:07:51 +0800

    Update LyPunter.sol

--- contracts/LyPunter.sol
+++ contracts/LyPunter.sol
@@ -10,6 +10,7 @@ contract LyPunter is Ownable {
         uint256 issueNum;       // the issue number
         uint256 rewardLevel;    // the prize level, 1 for top prize, 2 for secondary prize
         uint256 rewardValue;    // the prize amount
+        uint256 nftId;
     }
 
     // instance of LyLottery
@@ -21,10 +22,13 @@ contract LyPunter is Ownable {
     mapping(uint256 => Prize) private _prizes;
     // cummulative prize withdrawed by each user
     mapping(address => uint256) private _cumPrizeByUser;
-    // un-redeemed prizes' nft
+    // un-redeemed prizes' nftid
     uint256[] private _unredeemPrize;
+    // all prizes' nftid
+    uint256[] private _allPrizes;
     // start index of unredeemed prize array, make un-redeemed prized compact
     uint private _startIdx;
+    uint256 private _maxIssueNum;
 
     event Redeem(uint256 indexed nftId, uint256 value);
 
@@ -46,7 +50,11 @@ contract LyPunter is Ownable {
         uint256 totalValue = 0;
         for (uint i = 0; i < ids.length; i++) {
             Prize storage aPrize = _prizes[ids[i]];
+            require(aPrize.issueNum == 0, 'SetRewardYet!');
+            _allPrizes.push(ids[i]);
+
             aPrize.issueNum = issueNum;
+            aPrize.nftId = ids[i];
 
             uint256 value = secondValue;
             if (i < topNum) {
@@ -67,6 +75,10 @@ contract LyPunter is Ownable {
             }
         }
         _issuePool[issueNum] = totalValue;
+
+        if (issueNum > _maxIssueNum) {
+            _maxIssueNum = issueNum;
+        }
     }
 
     function payReward(uint256 issueNum) public payable {
@@ -102,23 +114,70 @@ contract LyPunter is Ownable {
         require(success, "RedeemFailed");
     }
 
-    // return the number and prizes redeemable by the msg.sender
-    function getPrizesByUser() public view returns (uint, Prize[] memory) {
+    function getCummPrizeByUser() public view returns (uint256) {
         address user = msg.sender;
+        return _cumPrizeByUser[user];
+    }
+
+    // return the prize redeemable by the msg.sender
+    function getBalanceByUser() public view returns (uint256)
+    {
+        address user = msg.sender;
+        uint256 totalPrize = 0;
 
         uint start = _startIdx;
         uint unredeemSize = _unredeemPrize.length - start;
-        Prize[] memory redeemable = new Prize[](unredeemSize);
-        uint validNum = 0;
         for (uint i = start; i < start + unredeemSize; i++) {
             uint256 nftId = _unredeemPrize[i];
             if (_lottery.checkOwnership(user, nftId)) {
-                redeemable[validNum] = _prizes[nftId];
-                validNum++;
+                totalPrize += _prizes[nftId].rewardValue;
+            }
+        }
+        return totalPrize;
+    }
+
+    function getPrizesByUser() 
+        public 
+        view 
+        returns (Prize[] memory, uint256[] memory)
+    {
+        address user = msg.sender;
+        uint size = _allPrizes.length;
+
+        uint256[] memory myPrizes = new uint256[](size);
+        uint validPrizeNum = 0;
+        bool[] memory isIssueNums = new bool[](_maxIssueNum);
+        uint issueSize = 0;
+        for (uint i = 0; i < size; i++) {
+            uint256 nftId = _allPrizes[i];
+            if (_lottery.checkOwnership(user, nftId)) {
+                myPrizes[validPrizeNum] = nftId;
+                validPrizeNum++;
+
+                uint256 issueNum = _prizes[nftId].issueNum - 1;
+                // dedup
+                if (!isIssueNums[issueNum]) {
+                    isIssueNums[issueNum] = true;
+                    issueSize++;
+                }
             }
         }
 
-        return (validNum, redeemable);
-    }
+        // collect prizes by the user
+        Prize[] memory myValidPrizes = new Prize[](validPrizeNum);
+        for (uint i = 0; i < validPrizeNum; i++) {
+            myValidPrizes[i] = _prizes[myPrizes[i]];
+        }
 
+        // collect issue numbers this user won
+        uint256[] memory issueNums = new uint256[](issueSize);
+        uint idx = 0;
+        for (uint i=0; i<isIssueNums.length; i++) {
+            if (isIssueNums[i]) {
+                issueNums[idx] = i+1;
+                idx++;
+            }
+        }
+        return (myValidPrizes, issueNums);
+    }
 }
