3ceb76f034b71b639d705532c7d420d1adf0dbf4
RyanDGWEB3 <136770240+RyanDGWEB3@users.noreply.github.com>
2023-07-26 18:30:22 +0800

    Update check_link_balance.py

--- monitor/check_link_balance.py
+++ monitor/check_link_balance.py
@@ -8,7 +8,7 @@ def check_link_balance():
     link_abi = open('./monitor/LinkToken.ABI').read()
     link_token = w3.eth.contract(address=link_addr, abi=link_abi)
 
-    vrfv2_addr = '0x7D67652A6FED4B935bDc496A4717a9B9cf90e725'
+    vrfv2_addr = ''
     balance = link_token.functions.balanceOf(vrfv2_addr).call()
     balance = w3.from_wei(balance, 'ether')
     print('Link balance', balance)
