f006dfdeb98677c04f94d921c655a12edcbf8b89
RyanDGWEB3 <136770240+RyanDGWEB3@users.noreply.github.com>
2023-07-26 18:30:02 +0800

    Update tg_bot.py

--- monitor/tg_bot.py
+++ monitor/tg_bot.py
@@ -12,8 +12,8 @@ def tel_send_message(token, chat_id, text):
     return r
 
 def main():
-    chat_id = "-891499835"
-    token = "6308650676:AAF4IB5uaTZJ-YgVTXUlH3e8k_EBkI8GuCo"
+    chat_id = ""
+    token = ""
 
     text = "Sorry, more test for bot"
     tel_send_message(token, chat_id, text)
