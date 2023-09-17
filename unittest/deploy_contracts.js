185a5cfb2fbb9e2392d594754cf49bfcdff4d52c
RyanDGWEB3 <136770240+RyanDGWEB3@users.noreply.github.com>
2023-09-17 17:25:15 +0800

    Update deploy_contracts.js

--- unittest/deploy_contracts.js
+++ unittest/deploy_contracts.js
@@ -1,14 +1,19 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { user_pks } = require("./signer.json");
 
 describe("LuckyWeb contract", function() {
   it("Deployment 4 contracts", async function() {
     var url = 'http://localhost:8545';
     var provider = new ethers.providers.JsonRpcProvider(url);
-    const _dist1 = new ethers.Wallet('0x689af8efa8c651a91ad287602527f3af2fe9f6501a7ac4b061667b5a93e037fd', provider);
-    const _dist2 = new ethers.Wallet('0xde9be858da4a475276426320d5e9262ecfc3ba460bfac56360bfa6c4c28b4ee0', provider);
-    const _treasury = new ethers.Wallet('0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80', provider);
-    const _issuer = '0x8626f6940E2eb28930eFb4CeF49B2d1F2C9C1199';
+    const _signer1 = new ethers.Wallet(user_pks[0], provider);
+    const _signer2 = new ethers.Wallet(user_pks[1], provider);
+    const _signer3 = new ethers.Wallet(user_pks[2], provider);
+    const _signer4 = new ethers.Wallet(user_pks[3], provider);
+    const _dist1 = _signer3;
+    const _dist2 = _signer2;
+    const _treasury = _signer1;
+    const _issuer = _signer4.address;
 
     const LyLottery = await ethers.getContractFactory("LyLottery");
     const lottery = await LyLottery.deploy();
@@ -18,7 +23,7 @@ describe("LuckyWeb contract", function() {
     console.log('LyLottery deployed at:'+ lotteryAddr);
 
     const LyIssuer = await ethers.getContractFactory("LyIssuer");
-    constructorArgs = [_issuer, _dist1.address, lotteryAddr]
+    constructorArgs = [_treasury.address, _issuer, _dist1.address, lotteryAddr]
     const issue = await LyIssuer.deploy(...constructorArgs);
     //const issue = await LyIssuer.attach('');
     await issue.deployed();
