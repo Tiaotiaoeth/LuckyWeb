15f07e7a04c53c1b135f5a10f534570dac60a1c6
RyanDGWEB3 <136770240+RyanDGWEB3@users.noreply.github.com>
2024-01-02 21:07:14 +0800

    FIX: contract version

--- contracts/LyPunter.sol
+++ contracts/LyPunter.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts@4.0.0/access/Ownable.sol";
 import "./LyLottery.sol";
 
 contract LyPunter is Ownable {
